4b8e9713f6dbccae1cc88723b15c4c8305330fa2
Nick Hafkemeyer <mail@nhafkemeyer.com>
2020-09-02 11:24:46 +0200

    Basic sudoku solver

--- solver.py
+++ solver.py
@@ -1,67 +1,84 @@
-import numpy as np
+class SudokuSolver:
 
-def is_valid(board, number, x, y):
-    """[summary]
+    def __init__(self, board=None):
+        self.current_board = board
 
-    Args:
-        board (int[][]): [description]
-        number (int): [description]
-        x (int): [description]
-        y (int): [description]
-    """
 
-    # check if number already exists in row
-    if number in board[y]:
+    def solve(self, board=None):
+        """Computes a solution for a partially solved sudoku board.
+
+        Args:
+            board (int[][]): the sudoku board, if none is given the one provided to the class at initialization is used
+
+        Returns:
+            boolean: True, if the board could be solved, False else
+        """
+
+        if board is None:
+            if self.current_board is None:
+                raise ValueError
+            board = self.current_board
+
+
+        x, y = self._get_unassigned_cell(board) or (None, None)
+        if x is None or y is None:
+            return True
+        
+        for num in range(1,10):
+            if self._is_valid(board, num, x, y):
+                board[y][x] = num
+                if self.solve(board):
+                    return True
+                board[y][x] = 0
         return False
 
-    # check if number already exists in col
-    if number in [board[i][x] for i in range(9)]:
-        return False
 
-    subgrid_x = x // 3
-    subgrid_y = y // 3
+    def _is_valid(self, board, number, x, y):
+        """Checks whether a given number is valid on a given position in a given sudoku board.
 
-    # check if number exists in a 3x3 subgrid
-    if number in [board[i][j] for i in range(subgrid_y * 3, subgrid_y * 3 + 3) for j in range(subgrid_x * 3, subgrid_x * 3 + 3)]:
-        return False
+        Args:
+            board (int[][]): the board against which to check
+            number (int): the number to check for validity
+            x (int): x index of the position to check
+            y (int): y index of the position to check
 
-    return True
+        Returns:
+            boolean: True, is the number is valid, False else
+        """
 
-def get_unassigned_cell(board):
-    """[summary]
+        # check if number already exists in row and that is is not the number to check
+        if number in board[y] and board[y].index(number) != x:
+            return False
 
-    Args:
-        board ([type]): [description]
+        # check if number already exists in col and that is is not the number to check
+        if number in [board[i][x] for i in range(9) if i != y]:
+            return False
 
-    Returns:
-        [type]: [description]
-    """
-    for y in range(len(board)):
-        for x in range(len(board[y])):
-            if board[y][x] == 0:
-                return x, y
-    return None
+        subgrid_x = x // 3
+        subgrid_y = y // 3
 
+        # check if number exists in a 3x3 subgrid
+        if number in [board[i][j] for i in range(subgrid_y * 3, subgrid_y * 3 + 3) for j in range(subgrid_x * 3, subgrid_x * 3 + 3) if i != y and j != x]:
+            return False
 
-def solve(board, gui=False):
-    """[summary]
-
-    Args:
-        board ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
-
-    x, y = get_unassigned_cell(board) or (None, None)
-    if x is None or y is None:
         return True
-    
-    for num in range(1,10):
-        if is_valid(board, num, x, y):
-            board[y][x] = num
-            if solve(board):
-                return True
-            board[y][x] = 0
-    return False
+
+    def _get_unassigned_cell(self, board):
+        """Get an unassigned/empty cell on a given sudoku board
+
+        Args:
+            board (int[][]): the board on which to look for empty cells
+
+        Returns:
+            [type]: a tuple (x, y) with the coordinates of an empty cell, or None if there are no empty cells
+        """
+
+        for y in range(len(board)):
+            for x in range(len(board[y])):
+                if board[y][x] == 0:
+                    return x, y
+        return None
+
+
+
 
